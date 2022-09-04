63d310bdeeb9647d04ba286160a8ff4fa3b3fa9b
prasanth <106507229+mechneerd@users.noreply.github.com>
2022-09-04 08:39:41 +0530

    Update tip.js

--- tip.js
+++ tip.js
@@ -1,4 +1,5 @@
 food =prompt('how much the food')
 tipPercentage = prompt('How much is the tip')
 tip = food * tipPercentage // percentage in float
+tip = food * (tipPercentage/100) 
 console.log(tip)
